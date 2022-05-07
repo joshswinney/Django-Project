f10b2a4f2909be408a4453690b038ab36036f7d8
Joshua Swinney <joshua_swinney1@baylor.edu>
2022-05-06 23:34:42 -0500

    django

---
+++ .venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\joshs\desktop\py\learninglogsproject\.venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
